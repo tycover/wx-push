96eedcdbf953da8eeb919a850709383e0d3cf15b
tycover <126587398+tycover@users.noreply.github.com>
2023-07-17 16:16:23 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxa15bd1c57a15a604",
+  appsecret: "a2b9189846c3d7428d84436a78ff0126",
+  template_id: "-PBVgiD3EgxIu36Tb0cIDKwrdGG0qDZsx4JqKxwu7W4",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "重庆",
+  first_date: "2023-7-05",
 };
